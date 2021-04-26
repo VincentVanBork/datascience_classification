cb2525e72d0eaf2240948c0120d0a9276947deb5
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-26 22:46:15 +0200

    changes in the mnist

--- svm/mnist.py
+++ svm/mnist.py
@@ -83,137 +83,33 @@ X_scaled = scale(X)
 # train test split
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size = 0.3, train_size = 0.2 ,random_state = 10)
 
-# linear model
-
-model_linear = SVC(kernel='linear')
-model_linear.fit(X_train, y_train)
-
-# predict
-y_pred = model_linear.predict(X_test)
-
-
-
 # confusion matrix and accuracy
 
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
-# accuracy
-print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
 
-# cm
-print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
-
-# non-linear model
-# using rbf kernel, C=1, default value of gamma
-
-# model
-non_linear_model = SVC(kernel='rbf')
-
-# fit
-non_linear_model.fit(X_train, y_train)
-
-# predict
-y_pred = non_linear_model.predict(X_test)
-
-
-
-# confusion matrix and accuracy
-
-# accuracy
-print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
-
-# cm
-print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
-
-# creating a KFold object with 5 splits 
-folds = KFold(n_splits = 5, shuffle = True, random_state = 10)
-
-# specify range of hyperparameters
-# Set the parameters by cross-validation
-hyper_params = [ {'gamma': [1e-2, 1e-3, 1e-4],
-                     'C': [5,10]}]
-
-
-# specify model
-model = SVC(kernel="rbf")
-
-# set up GridSearchCV()
-model_cv = GridSearchCV(estimator = model, 
-                        param_grid = hyper_params, 
-                        scoring= 'accuracy', 
-                        cv = folds, 
-                        verbose = 1,
-                        return_train_score=True)      
-
-# fit the model
-model_cv.fit(X_train, y_train)
-
-# cv results
-cv_results = pd.DataFrame(model_cv.cv_results_)
-
-print(cv_results)
-
-# converting C to numeric type for plotting on x-axis
-cv_results['param_C'] = cv_results['param_C'].astype('int')
-
-# # plotting
-plt.figure(figsize=(16,8))
-
-# subplot 1/3
-plt.subplot(131)
-gamma_01 = cv_results[cv_results['param_gamma']==0.01]
-
-plt.plot(gamma_01["param_C"], gamma_01["mean_test_score"])
-plt.plot(gamma_01["param_C"], gamma_01["mean_train_score"])
-plt.xlabel('C')
-plt.ylabel('Accuracy')
-plt.title("Gamma=0.01")
-plt.ylim([0.60, 1])
-plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-plt.xscale('log')
-
-# subplot 2/3
-plt.subplot(132)
-gamma_001 = cv_results[cv_results['param_gamma']==0.001]
-
-plt.plot(gamma_001["param_C"], gamma_001["mean_test_score"])
-plt.plot(gamma_001["param_C"], gamma_001["mean_train_score"])
-plt.xlabel('C')
-plt.ylabel('Accuracy')
-plt.title("Gamma=0.001")
-plt.ylim([0.60, 1])
-plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-plt.xscale('log')
-
-
-# subplot 3/3
-plt.subplot(133)
-gamma_0001 = cv_results[cv_results['param_gamma']==0.0001]
-
-plt.plot(gamma_0001["param_C"], gamma_0001["mean_test_score"])
-plt.plot(gamma_0001["param_C"], gamma_0001["mean_train_score"])
-plt.xlabel('C')
-plt.ylabel('Accuracy')
-plt.title("Gamma=0.0001")
-plt.ylim([0.60, 1])
-plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-plt.xscale('log')
-
-
-# printing the optimal accuracy score and hyperparameters
-best_score = model_cv.best_score_
-best_hyperparams = model_cv.best_params_
-
-print("The best test score is {0} corresponding to hyperparameters {1}".format(best_score, best_hyperparams))
-
-# model with optimal hyperparameters
-
-# model
-model = SVC(C=10, gamma=0.001, kernel="rbf")
-
+# Linear
+model = SVC(kernel='linear')
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-# metrics
-print("accuracy", metrics.accuracy_score(y_test, y_pred), "\n")
-print(metrics.confusion_matrix(y_test, y_pred), "\n")
+print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
+print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
+
+# Optimal stuff RBF
+model = SVC(C=10, kernel="rbf")
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
+print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
+
+# Random stuff
+model = SVC(C=5, kernel="rbf")
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
+print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
+
+
