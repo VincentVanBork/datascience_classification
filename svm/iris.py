dee85e4d65e2f1147c8b6723e463f6aa38d854cb
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-04-26 22:30:58 +0200

    normalization for svm iris

--- svm/iris.py
+++ svm/iris.py
@@ -9,9 +9,13 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# TODO: Czy trzeba przeprowadzić normalizację?
+# Scale data to have mean 0 and variance 1 
+# which is importance for convergence of the neural network
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
 # train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, train_size = 0.2 ,random_state = 10)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size = 0.3, train_size = 0.2 ,random_state = 10)
 
 # linear model
 
