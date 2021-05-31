744d99fab85a8340c2ae7d424703479bed7d4155
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-05-31 12:20:00 +0200

    generating confussion matrix

--- svm/breast_cancer.py
+++ svm/breast_cancer.py
@@ -4,7 +4,24 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-# import some data to play with
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+
+def experiment(svc):
+    model_linear.fit(X_train, y_train)
+
+    # predict
+    y_pred = model_linear.predict(X_test)
+
+    # accuracy
+    print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
+
+    # Confussion matrix
+    plot_confusion_matrix(svc, X_test, y_test)
+    plt.savefig('confussion_matrix' + '.png')
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
 breast_cancer = datasets.load_breast_cancer()
 X = breast_cancer.data
 y = breast_cancer.target
@@ -16,19 +33,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, train
 # linear model
 
 model_linear = SVC(kernel='linear')
-model_linear.fit(X_train, y_train)
-
-# predict
-y_pred = model_linear.predict(X_test)
-
-# confusion matrix and accuracy
-
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-# accuracy
-print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
-
-# cm
-print(metrics.confusion_matrix(y_true=y_test, y_pred=y_pred))
-
-#TODO: Czy trzeba zobaczyć jeszcze wersję nie liniową?
+experiment(model_linear)
