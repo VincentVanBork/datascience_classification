78a49e8955abab1b9136c3c589c777d7162e6bb2
Jakub Mroczek <jakubmroczek2@gmail.com>
2021-05-31 12:30:46 +0200

    generating roc

--- svm/breast_cancer.py
+++ svm/breast_cancer.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import classification_report, plot_confusion_matrix, plot_roc_curve
 
 def experiment(svc):
     model_linear.fit(X_train, y_train)
@@ -13,12 +13,15 @@ def experiment(svc):
     # predict
     y_pred = model_linear.predict(X_test)
 
-    # accuracy
-    print("accuracy:", metrics.accuracy_score(y_true=y_test, y_pred=y_pred), "\n")
-
     # Confussion matrix
     plot_confusion_matrix(svc, X_test, y_test)
     plt.savefig('confussion_matrix' + '.png')
+
+    # Prescision, accuracy, sensitivity and specifity
+    print("report:", metrics.classification_report(y_true=y_test, y_pred=y_pred), "\n")
+
+    metrics.plot_roc_curve(svc, X_test, y_test)
+    plt.savefig('roc.png')
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
