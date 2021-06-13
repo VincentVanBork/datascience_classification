65aa45daf258f3b1643b9f202c269f58fed47fc9
VincentVanBork <michal.darowny@gmail.com>
2021-06-13 10:39:08 +0200

    zmiany w metrics for evaluting method

--- neural_net/metrics.py
+++ neural_net/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix, roc_curve
 import tensorflow as tf
 from neural_net.cancer_neural import CancerModel
 from neural_net.inputs import cancer_neural_data_input, cancer_get_model_setup, \
@@ -44,6 +45,16 @@ def cancer_metrics():
     print("SPECIFITY",
           specificity(output_values.numpy().astype(dtype="float64"),
                       predictions.numpy().astype(dtype="float64")))
+    print("_____________ ROCC _________")
+    print(labels_1d)
+    print(rounded_predictions)
+    fpr, tpr, thresholds = roc_curve(labels_1d, rounded_predictions)
+
+    # plot the roc curve for the model
+    fig_roc_cancer, ax_roc_cancer = plt.subplots()
+    fig_roc_cancer.suptitle("ROC dla zbioru cancer")
+    ax_roc_cancer.plot(fpr, tpr, linestyle='--', label='No Skill')
+    plt.show()
 
 
 def iris_metrics():
@@ -82,11 +93,32 @@ def iris_metrics():
     print("SENSITIVITY",
           sensitivity(output_values.numpy().astype(dtype="float64"),
                       predictions.numpy().astype(dtype="float64")))
-    print("IRIS OUT", output_values)
-    print("IRIS PRED",predictions)
+    # pri
     print("SPECIFITY",
           specificity(output_values.numpy().astype(dtype="float64"),
                       predictions.numpy().astype(dtype="float64")))
+    print("_____________ ROCC _________")
+    print(labels_1d)
+    print(rounded_predictions)
+    fig_roc_iris, ax_roc_iris = plt.subplots()
+    fig_roc_iris.suptitle("ROC dla zbioru iris")
+
+    fpr, tpr, thresholds = roc_curve(
+        [1 if label == 0 else 0 for label in labels_1d],
+        [1 if label == 0 else 0 for label in rounded_predictions])
+    ax_roc_iris.plot(fpr, tpr, linestyle='--', label='1')
+
+    fpr, tpr, thresholds = roc_curve(
+        [1 if label == 1 else 0 for label in labels_1d],
+        [1 if label == 1 else 0 for label in rounded_predictions])
+    ax_roc_iris.plot(fpr, tpr, linestyle='--', label='2')
+    fpr, tpr, thresholds = roc_curve(
+        [1 if label == 2 else 0 for label in labels_1d],
+        [1 if label == 2 else 0 for label in rounded_predictions])
+    ax_roc_iris.plot(fpr, tpr, linestyle='--', label='3')
+
+    plt.show()
+
 
 def mnist_metrics():
     print("______________________")
@@ -100,8 +132,8 @@ def mnist_metrics():
     for input_tensor, output_values in test_mnist:
         predictions = model(input_tensor, training=False)
         # print(len(predictions))
-    print(output_values)
-    print(predictions)
+    # print(output_values)
+    # print(predictions)
     rounded_predictions = np.argmax(predictions, axis=-1)
 
     labels_1d = output_values.numpy()
@@ -131,9 +163,28 @@ def mnist_metrics():
           specificity(output_values.numpy().astype(dtype="float64"),
                       rounded_predictions.astype(dtype="float64")))
 
+    print("_____________ ROCC _________")
+    print(labels_1d)
+    print(rounded_predictions)
+    # plot the roc curve for the model
+    fig_roc_mnist, ax_roc_mnist = plt.subplots()
+    fig_roc_mnist.suptitle("ROC dla zbioru mnist")
+    for i in range(10):
+        if i == 0:
+            fpr, tpr, thresholds = roc_curve(
+                [1 if label == 0 else 0 for label in labels_1d],
+                [1 if label == 0 else 0 for label in rounded_predictions])
+            ax_roc_mnist.plot(fpr, tpr, linestyle='--', label='1')
+        else:
+            fpr, tpr, thresholds = roc_curve(
+                [1 if label == i else 0 for label in labels_1d],
+                [1 if label == i else 0 for label in rounded_predictions])
+            print(max(fpr), max(tpr))
+            ax_roc_mnist.plot(fpr, tpr, linestyle='--', label='1')
+    plt.show()
 
 if __name__ == "__main__":
     ...
-    cancer_metrics()
-    iris_metrics()
+    # cancer_metrics()
+    # iris_metrics()
     mnist_metrics()
